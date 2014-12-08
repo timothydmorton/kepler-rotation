ac4db04bcbceaa7e59b61563c08b05a401a63a9f
Timothy Morton <tim.morton@gmail.com>
2014-12-08 14:28:23 -0500

    added files

---
+++ keprot/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.1'
+
+try:
+    __KEPROT_SETUP__
+except NameError:
+    __KEPROT_SETUP__ = False
+
+if not __KEPROT_SETUP__:
+    __all__ = []
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+# Hackishly inject a constant into builtins to enable importing of the
+# package before the library is built.
+import sys
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
+builtins.__KEPROT_SETUP__ = True
+import keprot
+
+
+setup(name = "keprot",
+    version = isochrones.__version__,
+    description = "Estimating rotation periods from Kepler photometry",
+    long_description = readme(),
+    author = "Timothy D. Morton",
+    author_email = "tim.morton@gmail.com",
+    url = "https://github.com/timothydmorton/kepler-rotation",
+    packages = find_packages(),
+    #package_data = {'':['data/*']},
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Astronomy'
+      ],
+    #install_requires=['plotutils','pandas>=0.14','astropy>=0.3','emcee>=2.0'],
+    zip_safe=False
+) 
